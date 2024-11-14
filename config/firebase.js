05b1a71107cd6fb026e94525ce5187d838e3d35f
gachezra <gachomoezra@gmail.com>
2024-11-14 17:24:19 +0300

    Service account fixed

--- config/firebase.js
+++ config/firebase.js
@@ -2,8 +2,16 @@ const { getFirestore } = require('firebase-admin/firestore');
 const { getAuth } = require('firebase-admin/auth');
 const { getDatabase } = require('firebase-admin/database');
 const admin = require("firebase-admin");
-const serviceAccount = require("./pijiji-admin-sdk.json");
+const Buffer = require('buffer');
 require('dotenv').config();
+const base64Key = process.env.GOOGLE_SERVICE_KEY;
+
+if (!base64Key) {
+  throw new Error("Service account key is not set in environment variables");
+}
+
+const serviceAccount = JSON.parse(Buffer.from(base64Key, 'base64').toString('utf8'));
+
 
 const app = admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
